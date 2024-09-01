3c5321e16690f987118db1ff778a502a62ef1f23
dnyaneshwari12383 <dnyaneshwaritelange@gmail.com>
2024-09-01 18:53:08 +0530

    test

---
+++ .new.py
@@ -0,0 +1,23 @@
+def simple_intrest(p,n,r):
+    """
+    This function takes following inputs
+    p = Principal in INR
+    n = Number of years
+    r = Rate of Intrest in 
+    Output = Intrest and amount
+    """
+    I = (p*n*r)/100
+    A = p + I
+    return I, A
+
+# Take input from the user
+p = float(input("please enter principle in INR:"))
+n = float(input("please enter number of years"))
+r = float(input("Please enter rate of intrest in %p.a : "))
+
+# Call the function
+I, A = simple_intrest(p, n, r)
+
+# Print the results
+print(f"Simple Intrest : {I:.2f} INR")
+print(f"Amount : {A:.2f} INR")
