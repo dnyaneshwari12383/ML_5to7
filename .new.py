00627b993fd53b1dae8e82ee7506bceb24b75938
dnyaneshwari12383 <dnyaneshwaritelange@gmail.com>
2024-09-21 17:04:41 +0530

    updated

--- .new.py
+++ .new.py
@@ -1,23 +1,37 @@
-def simple_intrest(p,n,r):
-    """
-    This function takes following inputs
-    p = Principal in INR
-    n = Number of years
-    r = Rate of Intrest in 
-    Output = Intrest and amount
-    """
-    I = (p*n*r)/100
-    A = p + I
-    return I, A
-
-# Take input from the user
-p = float(input("please enter principle in INR:"))
-n = float(input("please enter number of years"))
-r = float(input("Please enter rate of intrest in %p.a : "))
-
-# Call the function
-I, A = simple_intrest(p, n, r)
-
-# Print the results
-print(f"Simple Intrest : {I:.2f} INR")
-print(f"Amount : {A:.2f} INR")
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def simple_intrest(p,n,r):\n",
+    "   \"\"\"\n",
+    "   This function takes following inputs\n",
+    "    p = Principal in INR\n",
+    "    n = Number of years\n",
+    "    r = Rate of Intrest in \n",
+    "    Output = Intrest and amount\n",
+    "    \"\"\"\n",
+    "    I = (p*n*r)/100\n",
+    "    A = p + I\n",
+    "    return I, A"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
